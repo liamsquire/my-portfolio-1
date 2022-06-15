d0ebbcfebd7eca97abd4343f8edeee1874c89adb
liamsquire <liamsquire@gmail.com>
2022-06-15 18:40:24 +0100

    Update to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,21 +8,21 @@ function initFontAwesome() {
   
   export default function Footer() {
     return (
-    <div>
- 
-      <div className="py-12 bg-gray-100">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="mt-4">
+      <hr className="h-2 bg-gradient-to-br bg-gradient-to-r from-pink-600 to-red-500"/>
+      <div className="py-12 bg-navy">
+      <div className="bg-gray max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
         <div className="text-center mt-2 ">
-            <p className="text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl mb-4">Thank you</p>
-            <p className="text-lg leading-6 font-medium text-gray-900">Thanks for stopping by. Feel free to get in touch.</p>
+            <p className="text-3xl leading-8 font-extrabold tracking-tight  sm:text-4xl mb-4">Thank you</p>
+            <p className="text-lg leading-6 font-medium ">Thanks for stopping by. Feel free to get in touch.</p>
         </div>
 
 
-        <div className="mt-10">
+        {/* <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-8 md:gap-y-10">
 
-{/* Begin Col */}
+{/* Begin Col *}
               <div className="relative">
                 <dt>
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl mb-10">Thank you</p>
@@ -30,9 +30,9 @@ function initFontAwesome() {
                 </dt>
                 <dd className="mt-2 text-base text-gray-500">feature.description</dd>
               </div>
-{/* End Col */}     
+{/* End Col *}     
 
-{/* Begin Col */}
+{/* Begin Col *}
 <div className="relative">
                 <dt>
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl mb-10">Thank you</p>
@@ -40,9 +40,9 @@ function initFontAwesome() {
                 </dt>
                 <dd className="mt-2 text-base text-gray-500">feature.description</dd>
               </div>
-{/* End Col */}     
+{/* End Col *}     
 
-{/* Begin Col */}
+{/* Begin Col *}
 <div className="relative">
                 <dt>
                     <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl mb-10">Thank you</p>
@@ -50,14 +50,17 @@ function initFontAwesome() {
                 </dt>
                 <dd className="mt-2 text-base text-gray-500">feature.description</dd>
               </div>
-{/* End Col */}     
+{/* End Col *}     
 
 
 
           </dl>
-        </div>
+        </div> */}
       </div>
     </div>
+    <div className="py-4 bg-black text-white">
+        Footer
+      </div>
       </div>
     )
   }

f87161ffa8b3f4a54cedb58d5f84d9b7e46bd611
liamsquire <liamsquire@gmail.com>
2022-06-15 18:41:48 +0100

    Import certifications component. Also imported new font awesome icons for portfolio and certifications components..

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import logo from './assets/logo-1.svg';
 import './App.css';
 import Portfolio from './components/Portfolio.js';
 import Header from './components/Header.js';
+import Certifications from './components/Certifications.js';
 import Footer from './components/Footer.js'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas, faLocationDot, faImage, faCloudArrowDown, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
-import { faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faPhp } from '@fortawesome/free-brands-svg-icons'
-library.add(faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faLocationDot, faPhp, faImage, faCloudArrowDown, faBars, faXmark)
+import { fas, faLocationDot, faImage, faCloudArrowDown, faBars, faXmark, faDatabase, faWandMagicSparkles, faAlignLeft, faAward } from '@fortawesome/free-solid-svg-icons'
+import { faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faPhp, faBootstrap, faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons'
+library.add(faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faLocationDot, faPhp, faImage, faCloudArrowDown, faBars, faXmark, faDatabase, faBootstrap, faWandMagicSparkles, faAlignLeft, faGoogle, faMicrosoft, faAward)
+
+const TITLE = 'My Page Title'
 
 export default function App() {
   return (
     <div id="page--container">
+      <title>{ TITLE }</title>
       <Header />
       <Portfolio />
+      <Certifications />
       <Footer />
     </div>
   )

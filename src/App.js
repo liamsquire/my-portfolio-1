4f423b74b66f35952906867c1be24063e53c138c
liamsquire <liamsquire@gmail.com>
2022-06-17 12:00:05 +0100

    Removed unused items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './assets/logo-1.svg';
+
 import './App.css';
 import Portfolio from './components/Portfolio.js';
 import Header from './components/Header.js';
@@ -10,12 +11,9 @@ import { fas, faLocationDot, faImage, faCloudArrowDown, faBars, faXmark, faDatab
 import { faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faPhp, faBootstrap, faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons'
 library.add(faTwitterSquare, faFacebook, faLinkedin, faGithub, faJs, faWordpress, faHtml5, faCss3Alt, faLocationDot, faPhp, faImage, faCloudArrowDown, faBars, faXmark, faDatabase, faBootstrap, faWandMagicSparkles, faAlignLeft, faGoogle, faMicrosoft, faAward)
 
-const TITLE = 'My Page Title'
-
 export default function App() {
   return (
     <div id="page--container">
-      <title>{ TITLE }</title>
       <Header />
       <Portfolio />
       <Certifications />
